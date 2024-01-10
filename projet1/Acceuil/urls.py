1f1f8fc53ee83ff06139659af741867b88554ad7
Amzaomar <cheikhomarn382@gmail.com>
2024-01-10 15:26:47 +0000

    mise a jour

--- projet1/Acceuil/urls.py
+++ projet1/Acceuil/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from Acceuil import views
 
 urlpatterns = [
-    path('Acceuil/', views.acceuil, name="acceuil"),
+    path('', views.acceuil, name="acceuil"),
     path('', views.base, name="base"),
 ]
